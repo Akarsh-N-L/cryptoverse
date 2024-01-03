6bf9b6c6e2498e85d15c4b950bc3546e39ff9a58
Akarsh-N-L <65647232+Akarsh-N-L@users.noreply.github.com>
2024-01-03 19:52:58 +0530

    removing Api key

--- src/services/cryptoAPI.ts
+++ src/services/cryptoAPI.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseUrl = "https://coinranking1.p.rapidapi.com";
 const headers = {
-  "X-RapidAPI-Key": "c59aaa0235mshc640aafd8be092ap17028cjsn774461af6e7f",
+  "X-RapidAPI-Key": "",
   "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
 };
 
@@ -19,21 +19,3 @@ export const cryptoAPI = createApi({
 });
 
 export const { useGetCryptosQuery } = cryptoAPI;
-
-// const options = {
-//     method: 'GET',
-//     url: 'https://coinranking1.p.rapidapi.com/coins',
-//     params: {
-//       referenceCurrencyUuid: 'yhjMzLPhuIDl',
-//       timePeriod: '24h',
-//       'tiers[0]': '1',
-//       orderBy: 'marketCap',
-//       orderDirection: 'desc',
-//       limit: '50',
-//       offset: '0'
-//     },
-//     headers: {
-//       'X-RapidAPI-Key': 'c59aaa0235mshc640aafd8be092ap17028cjsn774461af6e7f',
-//       'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
-//     }
-//   };
